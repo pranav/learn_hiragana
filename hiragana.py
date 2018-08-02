2f37c13d61583035d130871643fb6f6d2340e586
Pranav Gandhi <pgandhi@hubspot.com>
2018-08-01 20:14:49 -0400

    add args for starting and ending positions

--- hiragana.py
+++ hiragana.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+import argparse
 import random
 import logging
 import sys
@@ -106,6 +107,21 @@ HIRAGANA = [
     ('ぽ', 'po')
 ]
 
+def get_hiragana(starting, ending):
+    filtered_list = []
+    found_starting = False
+    found_ending = False
+    global HIRAGANA
+    for hiragana in HIRAGANA:
+        if found_starting and found_ending:
+            break
+        if hiragana[1] == starting:
+            found_starting = True
+        if hiragana[1] == ending:
+            found_ending = True
+        if found_starting or found_ending:
+            filtered_list.append(hiragana)
+    return filtered_list
 
 def ok(str):
     logging.info(Colors.OKGREEN + str + Colors.ENDC)
@@ -138,17 +154,24 @@ def log_current_trial_info(successful_trials, failed_trials):
     logging.info("%s/%s correct. %d%%" % (len(successful_trials), total_trials, int(len(successful_trials)*100/total_trials)))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--starting", default="a", required=False, dest="starting", help="Hiragana to start with")
+    parser.add_argument("--ending", default="po", required=False, dest="ending", help="Hiragana to end with")
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
     logging.info("LEARN HIRAGANA!")
-    max_hiragana = int(raw_input("How many hiragana would you like to learn?: "))
     successful_trials = []
     failed_trials = []
-
+    hiragana_list = get_hiragana(args.starting, args.ending)
     try:
         logging.info("Please enter in the matching hiragana.")
         while True:
             logging.info(" ")
-            choice = random.choice(HIRAGANA[0:max_hiragana])
+            choice = random.choice(hiragana_list)
             if trial(choice):
                 successful_trials.append(choice)
                 ok("໒( ͡ᵔ ▾ ͡ᵔ )७ Correct!")
